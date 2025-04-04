9c30b7e7ae51e8513cf0733c8d67d71d1696371f
d4hirata <dhirata.yc@gmail.com>
2025-04-04 23:35:27 +0900

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 curl -L -O https://www.dropbox.com/sh/clqo7ekr0ysbrip/AACsp1F6JQVYxp2Ocvlbexf9a/drs-5.0.6.tar.gz?dl=0
-mv drs-5.0.6.tar.gz\?dl=0 drs-5.0.6.tar.gz
+
 tar xzvf drs-5.0.6.tar.gz
 mv drs-5.0.6 daq
-cp source/MakeFile daq
-cp source/DOFrame.h daq/include
 
+cp source/Makefile daq
+cp source/DOFrame.h daq/include
 cp source/cmdline.h daq/include
 cp source/drscmd.cpp daq/src
 
